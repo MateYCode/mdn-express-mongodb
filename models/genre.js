24ac9fdc3d6d2b6d0a489a380b13a5489755c5f6
Ignacio Alonso <naclonso@gmail.com>
2019-04-04 17:45:02 -0300

    mdn express tutorial. Crud mongodb project. This first version includes app connection to cloud database

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true,min:3,max:100},
+  }
+);
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre';
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
